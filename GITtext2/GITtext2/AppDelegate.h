2e053d0c024862d810e10eea77c4af3e68255ce9
myUnicorn <492402475@qq.com>
2016-07-17 23:39:40 +0800

    测试2

---
+++ GITtext2/GITtext2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GITtext2
+//
+//  Created by 李鑫 on 16/7/17.
+//  Copyright © 2016年 lixin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
