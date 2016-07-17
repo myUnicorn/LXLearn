4704ac8b6298db5f710f4b0fdd97629365a1d792
myUnicorn <492402475@qq.com>
2016-07-17 23:27:52 +0800

    初始化项目

---
+++ GITtest/GITtest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GITtest
+//
+//  Created by 李鑫 on 16/7/17.
+//  Copyright © 2016年 lixin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GITtest/GITtest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GITtest
+//
+//  Created by 李鑫 on 16/7/17.
+//  Copyright © 2016年 lixin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
